293fe9b77fd2e51c0761c4a72ae471aa5ae9e2ab
JBobda <J.BobdaT@gmail.com>
2021-02-11 15:15:51 -0600

    Added comments to the client side sign in detailing requirements

--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	// TODO: Anything you want to do when the page is loaded?
 });
 
+
+// In this function I will have to:
+// Validate that the Employee ID is numeric and not blank
+// Validate that the password is not blank
 function validateForm() {
 	// TODO: Validate the user input
 	return true;
