3db4b9f3da070e69a59f4031feb8cba09a31c1b4
Vincent Tran <vincenttran197120@gmail.com>
2021-03-09 17:06:05 -0600

    Changed placeholder html

--- src/main/java/edu/uark/registerapp/controllers/enums/ViewNames.java
+++ src/main/java/edu/uark/registerapp/controllers/enums/ViewNames.java
@@ -4,7 +4,7 @@ public enum ViewNames {
 	SIGN_IN("signIn", "/"),
 	MAIN_MENU("mainMenu"),
 	PRODUCT_DETAIL("productDetail"),
-	PLACE_HOLDER("placeholder"),
+	PLACE_HOLDER("placeHolder"),
 	EMPLOYEE_DETAIL("employeeDetail"),
 	PRODUCT_LISTING("productListing");
 	
