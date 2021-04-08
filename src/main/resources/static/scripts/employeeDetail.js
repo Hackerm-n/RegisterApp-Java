fe298fb06f96b225472673f204029cadd6178388
faups <lukefaupel@gmail.com>
2021-04-08 15:56:39 -0500

    added functionality to employeedetail

--- src/main/resources/static/scripts/employeeDetail.js
+++ src/main/resources/static/scripts/employeeDetail.js
@@ -1,13 +1,77 @@
 let hideEmployeeSavedAlertTimer = undefined;
 
 document.addEventListener("DOMContentLoaded", () => {
-	// TODO: Things that need doing when the view is loaded
+	document.getElementById('employeeSavedAlertModal').style.display = "none";
+	document.getElementById('employeeNotSavedAlertModal').style.display = "none";
+
+	document.getElementById("saveButton").addEventListener("click", onSave);
 });
 
-// Save
-function saveActionClick(event) {
-	// TODO: Actually save the employee via an AJAX call
-	displayEmployeeSavedAlertModal();
+function onSave() {
+	var firstName = document.getElementById("firstName");
+	var lastName = document.getElementById("lastName");
+	var password = document.getElementById("password");
+	var confirmPassword = document.getElementById("confirmPassword");
+
+	console.log('saving')
+
+	if (firstName.value === '') {
+		firstName.focus();
+		saveFail();
+	} else if (lastName.value === '') {
+		lastName.focus();
+		saveFail();
+	} else if (password.value === '') {
+		password.focus();
+		saveFail();
+	} else if (confirmPassword.value !== password.value) {
+		confirmPassword.focus();
+		saveFail();
+	} else {
+		saveSuccess();
+	}
+}
+
+function saveSuccess() {
+	document.getElementById('saveButton').disabled = true;
+	var employeeSaved = document.getElementById('employeeSavedAlertModal');
+	var employeeNotSaved = document.getElementById('employeeNotSavedAlertModal');
+	employeeSaved.style.display = "block";
+	employeeNotSaved.style.display = "none";
+	var employeeId = document.getElementById('employeeId').value;
+	var employeeIdIsDefined = (employeeId != null && employeeId.trim() !== '');
+	const saveActionUrl = ("/api/employee/"
+		+ (employeeIdIsDefined ? employeeId : ""));
+	const saveEmployeeRequest = {
+		id: employeeId,
+		firstName: document.getElementById("firstName"),
+		lastName: document.getElementById("lastName"),
+		password: document.getElementById("password"),
+		employeeType: document.getElementById("employeeType"),
+	};
+
+	if (employeeIdIsDefined) {
+		ajaxPut(saveActionUrl, saveEmployeeRequest, (callbackResponse) => {
+			document.getElementById('saveButton').disabled = false;
+			if (isSuccessResponse(callbackResponse)) {
+				displayEmployeeSavedAlertModal();
+			}
+		});
+	} else {
+		ajaxPost(saveActionUrl, saveEmployeeRequest, (callbackResponse) => {
+			document.getElementById('saveButton').disabled = false;
+			if (isSuccessResponse(callbackResponse)) {
+				displayEmployeeSavedAlertModal();
+			}
+		});
+	}
+}
+
+function saveFail() {
+	var employeeSaved = document.getElementById('employeeSavedAlertModal');
+	var employeeNotSaved = document.getElementById('employeeNotSavedAlertModal');
+	employeeSaved.style.display = "none";
+	employeeNotSaved.style.display = "block";
 }
 
 function displayEmployeeSavedAlertModal() {
